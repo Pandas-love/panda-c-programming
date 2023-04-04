9340455df48017097c7c59c6b00ae38497e5ca44
Pandas-love <jaymujumderiubat@gmail.com>
2023-04-04 13:51:08 +0600

    2.5Problem1.c

---
+++ panda-c-programming/2.5Problem1.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main()
+{
+ printf("Recently I heard that you've achived\n95%% marks in your exam.\nThis is brilliant!\nI wish you'll shine in your life!\tGood\nluck with all the barriers(/\\) in your life.");
+}
