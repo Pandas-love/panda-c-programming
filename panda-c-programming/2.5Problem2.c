254d071439219e57b2d66662413b2135f5127ef5
Pandas-love <jaymujumderiubat@gmail.com>
2023-04-04 14:32:37 +0600

    2.5Problem2.c

--- panda-c-programming/2.5Problem2.c
+++ panda-c-programming/2.5Problem2.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    
+    int a;
+    float b;
+    scanf("%d %f",&a,&b);
+    printf("%d + %0.0f = %0.0f\n",a,b,a+b);
+    printf("%d - %0.0f = %0.0f\n",a,b,a-b);
+    printf("%d * %0.0f = %0.0f\n",a,b,a*b);
+    printf("%d / %0.0f = %0.2f\n",a,b,a/b);
+    return 0;
 }
